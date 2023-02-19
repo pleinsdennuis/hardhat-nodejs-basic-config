4c244157df25f9d7b87ccecdbd54e03a3f7788fa
pleinsdennuis <45921101+pleinsdennuis@users.noreply.github.com>
2023-02-19 17:04:15 +0100

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,3 +1,4 @@
+// node js server script to interact with your smart contract
 import Web3 from 'web3';
 import artifacts from "./artifacts/contracts/Addition.sol/Addition.json" assert { type: "json"};
 
@@ -7,9 +8,10 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+// you can hardcode the abi and the bytecode if you want
 const abi = artifacts.abi;
 const bytecode = artifacts.bytecode;
-const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const contractAddress = /* contract address */ ;
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 const contract = new web3.eth.Contract(abi, contractAddress);
@@ -25,15 +27,9 @@ server.on('connection', (ws, req) => {
 	console.log('connected');
 	ws.on('message', data => {
 		const DATA = JSON.parse(data);
-		contract.methods
-		.addition(2, 8)
-		.call()
-		.then(result => {
-			const test = result;
-			console.log(result);
-		})
-		.catch(err => console.log(err));
 	});
+	
+	//use your smart contract's methods
 });
 
 app.listen(8080);
