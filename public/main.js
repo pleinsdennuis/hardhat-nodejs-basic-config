90a64a80386ceddb187cbbb20402eab3d09b592a
pleinsdennuis <45921101+pleinsdennuis@users.noreply.github.com>
2023-02-19 17:13:18 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,10 +1,6 @@
 'use strict';
 const port = 40510;
 const ws = new WebSocket(`ws://localhost:${port}`);
-const submitBtn = document.getElementById('submitBtn');
-
-const resultHTML = document.getElementById('result');
-const errHTML = document.getElementById('err');
 
 //ws
 ws.onopen = () => console.log('connected');
